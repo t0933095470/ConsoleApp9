1bcc3657c6b5d7df383b50703f9b03a12780e044
t0933095470 <thethao165@gmail.com>
2022-09-12 09:34:55 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Swithc case structure
+
+int i;
+Console.WriteLine("Please input an integer number");
+i = Convert.ToInt32(Console.ReadLine());
+switch (i%2)
+{
+    case 0:
+        Console.WriteLine("This is an even number.");
+        break;
+    case 1: Console.WriteLine("This is not an even number.");
+        break;
+    default: Console.WriteLine("Please input again");
+        break;
+}
+
